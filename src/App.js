8399c8b3eb0d8c685ac85522fcdae9e13050cf7a
Dipanshu Gupta <dipanshug124@gmail.com>
2021-06-16 22:16:49 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Navigation from './Components/Navigation'
+import Products from './Pages/Products';
+import Cart from './Pages/Cart';
+const App = () => {
+    return (
+        <>
+            <Router>
+                <Navigation />
+                <Switch>
+                    <Route path="/" component={Home} exact></Route>
+                    <Route path="/products" component={Products}></Route>
+                    <Route path="/cart" component={Cart}></Route>
+                </Switch>
+            </Router>
+        </>
+    )
 }
 
 export default App;
